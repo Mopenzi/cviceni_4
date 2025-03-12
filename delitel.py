108b8b717f344c5cfebe54f071e80416f93c8200
Mopenzi <merny.cichal@seznam.cz>
2025-03-12 17:15:36 +0100

    vytvoril jsem soubor delitel.py a vypracoval ukol

---
+++ delitel.py
@@ -0,0 +1,16 @@
+vysledek = list()
+start = 3
+stop = 9
+delitel = 3
+
+if isinstance(start, int) and isinstance(stop, int) and isinstance(delitel, int):
+    print(f"Zadaný rozsah: <{start}, {stop}>")
+
+    for cislo in range(start, stop + 1):
+        if cislo % delitel == 0:
+            vysledek.append(cislo)
+
+    print(f"čísla dělitelná {delitel}: {vysledek}")
+
+else:
+    print("Zadané vstupy musí být celá čísla.")
